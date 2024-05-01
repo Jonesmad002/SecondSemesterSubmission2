804b66acc22776b9bb84b4087dc74eec5a638970
Jonesmad002 <89795698+Jonesmad002@users.noreply.github.com>
2024-05-01 13:27:45 -0400

    Add files via upload

--- Loader.cs
+++ Loader.cs
@@ -8,6 +8,10 @@ public class Loader : MonoBehaviour
     //This is the index of the scene the LoaderZone will load
     [SerializeField]
     private int thisLoad;
+    [SerializeField]
+    private float x;
+    [SerializeField]
+    private float y;
 
     //Loads a scene based on the index
     private void loader(int sceneToLoad)
@@ -21,13 +25,22 @@ public class Loader : MonoBehaviour
             if(collision.tag == "SharedPlayer")
         {
             loader(thisLoad);
+            moveToEntrance(collision);
+
         }
+    }
+
+    private void moveToEntrance(Collider2D player)
+    {
+        player.transform.position = new Vector3(x, y, 0);
+
     }
 
     //Loads the first scene, for testing
     public void menuLoad()
     {
         loader(1);
+      
     }
 }
 
